8df9284385d7cc4eef60c7d078530382fc4185f8
Adam Harrison <adamdharrison@gmail.com>
2023-01-05 00:37:51 -0500

    Still working things out.

--- bin/server.rb
+++ bin/server.rb
@@ -9,9 +9,46 @@ require 'timeout'
 
 @temporary_directory = "tmp"
 @update_threshold = 3600
-@default_arguments = "--cachedir=cache --userdir #{@temporary_directory}/data --datadir #{@temporary_directory}/data --quiet --json"
+@default_arguments = "--cachedir=cache --quiet --json"
 
-class BadRequest < StandardError
+class HTTPError < StandardError
+  def initialize(e = nil) 
+    super(e)
+    @original = e if e
+  end
+  def original 
+    return @original 
+  end
+  def status_line 
+    return self.code.to_s + " " + self.status 
+  end
+  def response 
+    return [self.code, { "Content-Length" => self.status_line.size }, self.status_line] 
+  end
+end
+class BadRequestError < HTTPError
+  def code 
+    return 400 
+  end
+  def status 
+    return "Bad Request" 
+  end
+end
+class NotFoundError < HTTPError
+  def code 
+    return 404 
+  end
+  def status 
+    return "Not Found" 
+  end
+end
+class InternalServerError < HTTPError
+  def code 
+    return 500 
+  end
+  def status 
+    return "Internal Server Error" 
+  end
 end
 
 def log(request_number, type, message)
@@ -44,25 +81,26 @@ def handle_download(request_number, query, headers)
   platform = "x86_64-windows" if !platform && headers["user-agent"] =~ /Windows/
   platform = "x86_64-darwin" if !platform && headers["user-agent"] =~ /Macintosh/
   platform = "x86_64-linux" if !platform
-  arguments = "#{@default_arguments} --arch=#{platform}"
+  directory = "#{@temporary_directory}/#{request_number}"
+  arguments = "#{@default_arguments} --arch=#{platform}  --userdir #{directory}/data --datadir #{directory}/data"
 
-  raise BadRequest.new() if [platform, version, *plugins].select { |x| x =~ /[^a-z\.\-_0-9]/ }.first    
+  raise BadRequestError.new() if [platform, version, *plugins].select { |x| x =~ /[^a-z\.\-_0-9]/ }.first
 
   system_log(request_number, "./lpm lite-xl install #{version} #{arguments}", "can't find lite-xl #{version} for #{platform}")
   lite_xl = JSON.parse(system_log(request_number, "./lpm lite-xl list #{arguments}", "can't list lite-xls"))["lite-xls"].select { |x| x["version"] == version }.first
   raise "can't find lite-xl installed local #{version}" if !lite_xl || !lite_xl["local_path"]
-  FileUtils.cp_r(lite_xl["local_path"] + "/lite-xl", "#{@temporary_directory}/lite-xl")
-  FileUtils.cp_r(lite_xl["local_path"] + "/data", "#{@temporary_directory}/data")
+  FileUtils.cp_r(lite_xl["local_path"] + "/lite-xl", "#{directory}/lite-xl")
+  FileUtils.cp_r(lite_xl["local_path"] + "/data", "#{directory}/data")
   system_log(request_number, "./lpm install #{plugins.join(' ')} #{arguments}", "can't find plugins")
   if platform =~ /windows/
     filename = "lite-xl-#{version}-#{platform}.zip"
-    system_log(request_number, "cd #{@temporary_directory} && zip -r #{filename} lite-xl data && cd ..", "can't zip lite-xl")
+    system_log(request_number, "cd #{directory} && zip -r #{filename} lite-xl data && cd ..", "can't zip lite-xl")
   else
     filename = "lite-xl-#{version}-#{platform}.tar.gz"
-    system_log(request_number, "tar -C #{@temporary_directory} -zcvf #{@temporary_directory}/#{filename} lite-xl data", "can't tar lite-xl")
+    system_log(request_number, "tar -C #{directory} -zcvf #{directory}/#{filename} lite-xl data", "can't tar lite-xl")
   end
-  file = File.open("#{@temporary_directory}/#{filename}", "rb")
-  return [200, { "Content-Length" => File.size("#{@temporary_directory}/#{filename}"), "Content-Type" => "application/octet-stream", "Content-Disposition" => "attachment; filename=\"#{filename}\"" }, Proc.new {
+  file = File.open("#{directory}/#{filename}", "rb")
+  return [200, { "Content-Length" => File.size("#{directory}/#{filename}"), "Content-Type" => "application/octet-stream", "Content-Disposition" => "attachment; filename=\"#{filename}\"" }, Proc.new {
     file.read(8192)
   }]
 end
@@ -73,6 +111,8 @@ def handle_request(request_number, method, path, query, headers)
   elsif path == "/"
     body = File.read("index.html", encoding: "binary")
     return [200, { 'Content-Type' => 'text/html', "Content-Length" => body.size }, body]
+  else
+    raise NotFoundError.new()
   end
 end
 
@@ -95,37 +135,37 @@ while session = server.accept
   end
   total_requests += 1
   request_number = total_requests
+  FileUtils.mkdir("#{@temporary_directory}/#{request_number}")
   begin
     status = 200
     body = nil
     response_headers = {}
     begin 
-      request = nil
-      request_headers = {}
-      Timeout::timeout(5, StandardError, "Read Timeout") {  
-        request = session.gets(4096).chomp
-        log(request_number, "INFO", request.chomp)
-        while true
-          line = session.gets(4096)
-          break if line == "\r\n"
-          captures = line.match /^([\w\-_]+)\s*:\s*(.*?)\r\n$/
-          raise "can't parse header" unless captures && captures.size == 3
-          request_headers[captures[1].downcase] = captures[2]
-        end
-      }
-      method, full_path = request.split(' ')
-      path, query = full_path.split('?')
-      status, response_headers, body = handle_request(request_number, method, path, query || "", request_headers)
-    rescue BadRequestError => e
-      status = 400
-      body = "400 Bad Request"
-      response_headers = { "Content-Length" => body.size }
-      log(request_number, "ERROR", "Bad Request: #{e.backtrace.join("\n")}.")
-    rescue StandardError => e
-      status = 500
-      body = "500 Internal Server Error"
-      response_headers = { "Content-Length" => body.size }
-      log(request_number, "ERROR", "Internal Server Error: #{e.backtrace.join("\n")}.")
+      begin 
+        request = nil
+        request_headers = {}
+        Timeout::timeout(5, StandardError, "Read Timeout") {  
+          request = session.gets(4096).chomp
+          log(request_number, "INFO", request.chomp)
+          while true
+            line = session.gets(4096)
+            break if line == "\r\n"
+            captures = line.match /^([\w\-_]+)\s*:\s*(.*?)\r\n$/
+            raise "can't parse header" unless captures && captures.size == 3
+            request_headers[captures[1].downcase] = captures[2]
+          end
+        }
+        method, full_path = request.split(' ')
+        path, query = full_path.split('?')
+        status, response_headers, body = handle_request(request_number, method, path, query || "", request_headers)
+      rescue HTTPError => e
+        raise
+      rescue StandardError => e
+        raise InternalServerError.new(e)
+      end
+    rescue HTTPError => e
+      status, response_headers, body = e.response
+      log(request_number, "ERROR", e.status_line + ": #{(e.original || e).to_s} #{(e.original || e).backtrace.join("; ")}.")
     end
     Timeout::timeout(5, StandardError, "Read Timeout") {  
       session.print "HTTP/1.1 #{status}\r\n"
